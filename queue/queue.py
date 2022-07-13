dad5ffb965daa64dd2ed7772b4d4e56d6e89521c
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-07-13 20:44:37 +0300

    add queue

---
+++ queue/queue.py
@@ -0,0 +1,14 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.insert(0, item)
+
+    def dequeue(self):
+        if not self.queue:
+             return None
+        return self.queue.pop()
+
+    def size(self):
+        return len(self.queue) 
---
+++ queue/test_queue.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from queue import Queue
+
+class TestQueue(TestCase):
+    def setUp(self) -> None:
+        self.queue = Queue()
+
+    def test_queue(self):
+        for i in range(3):
+            self.queue.enqueue(i)
+        self.assertEqual(self.queue.size(), 3)
+        self.assertEqual(self.queue.dequeue(), 0)
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertEqual(self.queue.dequeue(), 2)
+        self.assertEqual(self.queue.size(), 0)
