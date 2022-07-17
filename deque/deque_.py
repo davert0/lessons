891bca2449e88c6b3bbab6f82812d2e018465cb4
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-07-17 23:32:15 +0300

    add deque

---
+++ deque/deque_.py
@@ -0,0 +1,23 @@
+class Deque:
+    def __init__(self):
+        self.dequeue = []
+        
+    def addFront(self, item):
+        self.dequeue.insert(0, item)
+
+    def addTail(self, item):
+        self.dequeue.append(item)
+
+    def removeFront(self):
+        if not self.dequeue:
+            return None
+        
+        return self.dequeue.pop(0)
+
+    def removeTail(self):
+        if not self.dequeue:
+            return None 
+        return self.dequeue.pop()
+
+    def size(self):
+        return len(self.dequeue)
---
+++ deque/test_deque.py
@@ -0,0 +1,16 @@
+# from unittest import TestCase
+# from dequeue_ import Deque
+# import queue
+
+# class TestQueue(TestCase):
+#     def setUp(self) -> None:
+#         self.queue = Queue()
+
+#     def test_queue(self):
+#         for i in range(3):
+#             self.queue.enqueue(i)
+#         self.assertEqual(self.queue.size(), 3)
+#         self.assertEqual(self.queue.dequeue(), 0)
+#         self.assertEqual(self.queue.dequeue(), 1)
+#         self.assertEqual(self.queue.dequeue(), 2)
+#         self.assertEqual(self.queue.size(), 0)
