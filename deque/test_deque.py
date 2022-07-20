6228d482c611515fa628162b5377540e640bd7f9
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-07-20 20:05:26 +0300

    add tests for deque

--- deque/test_deque.py
+++ deque/test_deque.py
@@ -1,16 +1,46 @@
-# from unittest import TestCase
-# from dequeue_ import Deque
-# import queue
+from unittest import TestCase
+from deque_ import Deque
 
-# class TestQueue(TestCase):
-#     def setUp(self) -> None:
-#         self.queue = Queue()
+class TestQueue(TestCase):
+    def setUp(self) -> None:
+        self.deque = Deque()
 
-#     def test_queue(self):
-#         for i in range(3):
-#             self.queue.enqueue(i)
-#         self.assertEqual(self.queue.size(), 3)
-#         self.assertEqual(self.queue.dequeue(), 0)
-#         self.assertEqual(self.queue.dequeue(), 1)
-#         self.assertEqual(self.queue.dequeue(), 2)
-#         self.assertEqual(self.queue.size(), 0)
+    def test_add_front(self):
+        self.deque.addFront(5)
+        self.deque.addFront(6)
+        self.assertEqual(self.deque.size(), 2)
+        self.assertIn(5, self.deque.dequeue)
+        self.assertIn(6, self.deque.dequeue)
+        self.assertEqual([6, 5], self.deque.dequeue)
+
+
+    def test_add_tail(self):
+        self.deque.addTail(7)
+        self.deque.addTail(8)
+        self.deque.addTail(9)
+        self.assertEqual(self.deque.size(), 3)
+        self.assertIn(7, self.deque.dequeue)
+        self.assertIn(8, self.deque.dequeue)
+        self.assertIn(9, self.deque.dequeue)
+        self.assertEqual([7, 8, 9], self.deque.dequeue)
+
+    def test_removeFront(self):
+        for i in range(3):
+            self.deque.addFront(i)
+        self.assertEqual(2, self.deque.removeFront())
+        self.assertEqual(2, self.deque.size())
+        self.assertEqual(1, self.deque.removeFront())
+        self.assertEqual(1, self.deque.size())
+        self.assertEqual(0, self.deque.removeFront())
+        self.assertEqual(None, self.deque.removeFront())
+
+
+    def test_removeTail(self):
+        for i in range(3):
+            self.deque.addFront(i)
+        self.assertEqual(0, self.deque.removeTail())
+        self.assertEqual(2, self.deque.size())
+        self.assertEqual(1, self.deque.removeTail())
+        self.assertEqual(1, self.deque.size())
+        self.assertEqual(2, self.deque.removeTail())
+        self.assertEqual(None, self.deque.removeTail())
