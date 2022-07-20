4e2c63ed49e1c28f44df41db60e8c7b07c861787
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-07-20 19:53:45 +0300

    add is_palyndrome

---
+++ deque/palindrome.py
@@ -0,0 +1,23 @@
+from deque_ import Deque
+import string
+
+def is_palindrome(text: str):
+    text = text.lower().replace(" ", "").translate(str.maketrans('', '', string.punctuation))
+
+    deque_front = Deque()
+    deque_back = Deque()
+    front = []
+    back = []
+    for letter in text:
+        deque_front.addTail(letter)
+    deque_back.dequeue = deque_front.dequeue.copy()
+
+    while deque_front.size() > 0:
+        front.append(deque_front.removeFront())
+
+    while deque_back.size() > 0:
+        back.append(deque_back.removeTail())
+
+    return back == front
+
+    
