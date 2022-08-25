0d83fbb17a2245644d8dbc618fbd780c75d775c0
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-08-25 21:27:25 +0300

    fix naming in palindrome

--- als1/deque/palindrome.py
+++ als1/deque/palindrome.py
@@ -3,7 +3,7 @@ import string
 
 
 def is_palindrome(text: str):
-    text = (
+    prepared_text = (
         text.lower()
         .replace(" ", "")
         .translate(str.maketrans("", "", string.punctuation))
@@ -11,16 +11,16 @@ def is_palindrome(text: str):
 
     deque_front = Deque()
     deque_back = Deque()
-    front = []
-    back = []
-    for letter in text:
+    front_reading = []
+    back_reading = []
+    for letter in prepared_text:
         deque_front.addTail(letter)
     deque_back.dequeue = deque_front.dequeue.copy()
 
     while deque_front.size() > 0:
-        front.append(deque_front.removeFront())
+        front_reading.append(deque_front.removeFront())
 
     while deque_back.size() > 0:
-        back.append(deque_back.removeTail())
+        back_reading.append(deque_back.removeTail())
 
-    return back == front
+    return back_reading == front_reading
