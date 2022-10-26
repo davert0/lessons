97a5d57b1a1568e2ea7dc4f2e16f151c7d9afc1d
Lint Action <lint-action@samuelmeuli.com>
2022-10-26 09:34:59 +0000

    Fix code style issues with Black

--- recursion/tests/test_resursive_sum.py
+++ recursion/tests/test_resursive_sum.py
@@ -1,16 +1,9 @@
 import pytest
 from sum import recursive_sum
 
+
 @pytest.mark.parametrize(
-    "input,expected", 
-    [
-        (12345, 15),
-        (5858, 26),
-        (0, 0),
-        (1, 1),
-        (2, 2),
-        (45, 9)
-    ]
+    "input,expected", [(12345, 15), (5858, 26), (0, 0), (1, 1), (2, 2), (45, 9)]
 )
 def test_recursive_sum(input, expected):
     assert recursive_sum(input) == expected
