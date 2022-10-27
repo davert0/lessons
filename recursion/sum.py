84b8f7749a87e4d79bf92d03f89d6018eac0022c
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-10-27 15:22:34 +0300

    fix recursive sum

--- recursion/sum.py
+++ recursion/sum.py
@@ -1,5 +1,4 @@
 def recursive_sum(n: int):
-    n = str(n)
-    if len(n) == 1:
-        return int(n)
-    return int(n[:1]) + recursive_sum(int(n[1:]))
+    if n < 10:
+        return n % 10
+    return n % 10 + recursive_sum(n//10)
