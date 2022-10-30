082986a7fb7b4bb386b8e44962f62a2b1c682856
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-10-30 12:28:05 +0300

    refactor

--- recursion/even_index_printer.py
+++ recursion/even_index_printer.py
@@ -5,4 +5,4 @@ def print_even_index(list_: list[Any]):
     if not list_:
         return
     print(list_[0])
-    return print_even_index(list_[2:])
+    print_even_index(list_[2:])
--- recursion/even_printer.py
+++ recursion/even_printer.py
@@ -4,4 +4,4 @@ def print_even(list_: list[int]):
     number = list_.pop()
     if number % 2 == 0:
         print(number)
-    return print_even(list_)
+    print_even(list_)
