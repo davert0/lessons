25556fb3e65f73ea2f058bf3e5ecaf07960226ae
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-11-01 14:08:46 +0300

    fix second max

--- recursion/second_max.py
+++ recursion/second_max.py
@@ -1,13 +1,13 @@
-
-def find_second_max(list_, first_max, second_max):
-    if not list_:
-        return second_max
-    if list_[0] >= first_max:
-        second_max = first_max
-        first_max = list_[0]
-    return find_second_max(list_[1:], first_max, second_max)
-
-
 def get_second_max(list_: list[int]):
+    
+    def find_second_max(index, first_max, second_max):
+        if len(list_) == index:
+            return second_max
+        if list_[index] >= first_max:
+            second_max = first_max
+            first_max = list_[index]
+        return find_second_max(index+1, first_max, second_max)
+
     first_max, second_max = (list_[0], list_[1]) if list_[0] > list_[1] else (list_[1], list_[0])
-    return find_second_max(list_[2:], first_max, second_max)
+
+    return find_second_max(2, first_max, second_max)
