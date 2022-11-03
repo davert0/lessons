95fe45c86bafe9e41d9a74cb0cdece9b4d4fe776
Lint Action <lint-action@samuelmeuli.com>
2022-11-03 14:00:25 +0000

    Fix code style issues with Black

--- als2/simple_tree.py
+++ als2/simple_tree.py
@@ -1,27 +1,27 @@
 class SimpleTreeNode:
     def __init__(self, val, parent):
         self.NodeValue = val
-        self.Parent = parent 
+        self.Parent = parent
         self.Children = []
-	
-class SimpleTree:
 
+
+class SimpleTree:
     def __init__(self, root):
         self.Root = root
-	
+
     def AddChild(self, ParentNode, NewChild):
         ParentNode.Children.append(NewChild)
-  
+
     def DeleteNode(self, NodeToDelete):
         NodeToDelete.Parent.Children.remove(NodeToDelete)
         NodeToDelete.Parent = None
-
 
     def GetAllNodes(self):
         def find_nodes(node):
             result.append(node)
             for children_node in node.Children:
                 find_nodes(children_node)
+
         result = []
         node = self.Root
         find_nodes(node)
@@ -33,16 +33,17 @@ class SimpleTree:
                 result.append(node)
             for children_node in node.Children:
                 find_nodes(children_node)
+
         result = []
         node = self.Root
         find_nodes(node)
         return result
-   
+
     def MoveNode(self, OriginalNode, NewParent):
         OriginalNode.Parent.Children.remove(OriginalNode)
         OriginalNode.Parent = NewParent
         NewParent.Children.append(OriginalNode)
-   
+
     def Count(self):
         return len(self.GetAllNodes())
 
--- als2/tests/test_simple_tree.py
+++ als2/tests/test_simple_tree.py
@@ -2,9 +2,11 @@ from logging import root
 import pytest
 from simple_tree import SimpleTree, SimpleTreeNode
 
+
 @pytest.fixture
 def root_node():
     return SimpleTreeNode(0, None)
+
 
 @pytest.fixture
 def tree(root_node):
@@ -16,10 +18,12 @@ def child_node(root_node):
     new_child = SimpleTreeNode(1, root_node)
     return new_child
 
+
 @pytest.fixture
 def tree_with_two_nodes(tree, root_node, child_node):
     tree.AddChild(root_node, child_node)
     return tree
+
 
 @pytest.fixture
 def tree_with_six_nodes(tree, root_node):
@@ -35,6 +39,7 @@ def tree_with_six_nodes(tree, root_node):
     tree.AddChild(child_2, child_5)
     return tree
 
+
 @pytest.fixture
 def tree_with_doubling_nodes(tree_with_six_nodes, root_node):
     child_1 = SimpleTreeNode(1, root_node)
@@ -44,6 +49,7 @@ def tree_with_doubling_nodes(tree_with_six_nodes, root_node):
     tree_with_six_nodes.AddChild(child_1, child_2)
     tree_with_six_nodes.AddChild(root_node, child_3)
     return tree_with_six_nodes
+
 
 def test_add_child(tree, root_node):
     new_child = SimpleTreeNode(1, root_node)
@@ -57,6 +63,7 @@ def test_delete_node(tree_with_two_nodes, child_node):
     assert child_node.Parent == None
     assert tree_with_two_nodes.Root.Children == []
 
+
 def test_get_all_nodes(tree_with_six_nodes):
     assert len(tree_with_six_nodes.GetAllNodes()) == 6
     nodes_values = [node.NodeValue for node in tree_with_six_nodes.GetAllNodes()]
@@ -68,6 +75,7 @@ def test_find_nodes_by_value(tree_with_doubling_nodes):
     assert len(tree_with_doubling_nodes.FindNodesByValue(0)) == 1
     assert len(tree_with_doubling_nodes.FindNodesByValue(1)) == 2
     assert len(tree_with_doubling_nodes.FindNodesByValue(2)) == 3
+
 
 def test_move_node(tree, root_node):
     child_1 = SimpleTreeNode(1, root_node)
@@ -86,11 +94,14 @@ def test_move_node(tree, root_node):
     assert child_1 in child_3.Children
     assert child_1.Parent == child_3
 
+
 def test_count_six(tree_with_six_nodes):
     assert tree_with_six_nodes.Count() == 6
 
+
 def test_count_nine(tree_with_doubling_nodes):
     assert tree_with_doubling_nodes.Count() == 9
+
 
 def test_leaf_count(tree_with_six_nodes):
     assert tree_with_six_nodes.LeafCount() == 3
