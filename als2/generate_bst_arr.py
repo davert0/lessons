2e6f491642149e4cec58d6e6b49dad18cb0fe976
Lint Action <lint-action@samuelmeuli.com>
2022-11-28 20:45:22 +0000

    Fix code style issues with Black

--- als2/generate_bst_arr.py
+++ als2/generate_bst_arr.py
@@ -1,14 +1,14 @@
 def GenerateBBSTArray(a):
     a = sorted(a)
     res = [None] * len(a)
-    middle = len(a)//2
+    middle = len(a) // 2
     index = 0
     res[index] = a[middle]
     queue = [a[middle:], a[:middle]]
-    index +=1 
+    index += 1
     while index < len(res):
         sub_arr = queue.pop()
-        middle = len(sub_arr)//2
+        middle = len(sub_arr) // 2
         res[index] = sub_arr[middle]
         queue.insert(0, sub_arr[:middle])
         queue.insert(0, sub_arr[middle:])
