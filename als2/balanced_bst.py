151d708362416ed79a6d0d08025fa3343214c58c
Lint Action <lint-action@samuelmeuli.com>
2022-12-03 17:49:36 +0000

    Fix code style issues with Black

--- als2/balanced_bst.py
+++ als2/balanced_bst.py
@@ -1,21 +1,20 @@
 class BSTNode:
-	
     def __init__(self, key, parent):
-        self.NodeKey = key # ключ узла
-        self.Parent = parent # родитель или None для корня
-        self.LeftChild = None # левый потомок
-        self.RightChild = None # правый потомок
-        self.Level = 0 # уровень узла
-        
+        self.NodeKey = key  # ключ узла
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+        self.Level = 0  # уровень узла
+
+
 class BalancedBST:
-		
     def __init__(self):
-        self.Root = None # корень дерева
-  
+        self.Root = None  # корень дерева
+
     def GenerateTree(self, a):
-	# создаём дерево с нуля из неотсортированного массива a
+        # создаём дерево с нуля из неотсортированного массива a
         a = sorted(a)
-        middle_index = len(a)//2
+        middle_index = len(a) // 2
         middle_key = a[middle_index]
         self.Root = BSTNode(middle_key, None)
         self.generate(self.Root, a[:middle_index], 1)
@@ -33,17 +32,17 @@ class BalancedBST:
             if node.NodeKey > parent.NodeKey:
                 parent.RightChild = node
             return
-        middle_index = len(a)//2
+        middle_index = len(a) // 2
         middle_key = a[middle_index]
         node = BSTNode(middle_key, parent)
         if node.NodeKey < parent.NodeKey:
             parent.LeftChild = node
             print(node.NodeKey)
-        if node.NodeKey > parent.NodeKey: # 3 5 8 10 12 15 18
+        if node.NodeKey > parent.NodeKey:  # 3 5 8 10 12 15 18
             parent.RightChild = node
         node.Level = level
-        node.LeftChild = self.generate(node, a[:middle_index], level+1)
-        node.RightChild = self.generate(node, a[middle_index:], level+1)
+        node.LeftChild = self.generate(node, a[:middle_index], level + 1)
+        node.RightChild = self.generate(node, a[middle_index:], level + 1)
 
     def IsBalanced(self, root_node):
         return False
