3000c8b6c4178dba1b24a6b6bf121dbbe752600b
Lint Action <lint-action@samuelmeuli.com>
2022-12-04 20:57:41 +0000

    Fix code style issues with Black

--- als2/balanced_bst.py
+++ als2/balanced_bst.py
@@ -1,19 +1,18 @@
 class BSTNode:
-	
     def __init__(self, key, parent):
-        self.NodeKey = key # ключ узла
-        self.Parent = parent # родитель или None для корня
-        self.LeftChild = None # левый потомок
-        self.RightChild = None # правый потомок
-        self.Level = 0 # уровень узла
-        
+        self.NodeKey = key  # ключ узла
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+        self.Level = 0  # уровень узла
+
+
 class BalancedBST:
-		
     def __init__(self):
-        self.Root = None # корень дерева
-  
+        self.Root = None  # корень дерева
+
     def GenerateTree(self, a):
-	# создаём дерево с нуля из неотсортированного массива a
+        # создаём дерево с нуля из неотсортированного массива a
 
         a = sorted(a)
         self.generate(a, None, 0)
@@ -26,17 +25,25 @@ class BalancedBST:
             self.Root = BSTNode(a[0], parent)
             self.Root.Level = level
             return self.Root
-        middle_index = len(a)//2
+        middle_index = len(a) // 2
         root_key = a[middle_index]
         self.Root = BSTNode(root_key, parent)
         self.Root.Level = level
-        self.Root.LeftChild = BalancedBST().generate(a[:middle_index], self.Root, level+1)
-        self.Root.RightChild = BalancedBST().generate(a[middle_index:], self.Root, level+1)
+        self.Root.LeftChild = BalancedBST().generate(
+            a[:middle_index], self.Root, level + 1
+        )
+        self.Root.RightChild = BalancedBST().generate(
+            a[middle_index:], self.Root, level + 1
+        )
         return self.Root
 
     def IsBalanced(self, root_node):
         if root_node.LeftChild and root_node.RightChild:
-            return True and self.IsBalanced(root_node.LeftChild) and self.IsBalanced(root_node.RightChild)
+            return (
+                True
+                and self.IsBalanced(root_node.LeftChild)
+                and self.IsBalanced(root_node.RightChild)
+            )
         if root_node.LeftChild and root_node.LeftChild.LeftChild:
             return False
         if root_node.RightChild and root_node.RightChild.RightChild:
