2a5e24b265cde661ac33576ff1b249bb96f739ae
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2022-12-13 23:56:36 +0300

    add heap

--- als2/heap.py
+++ als2/heap.py
@@ -5,48 +5,83 @@ class Heap:
     def MakeHeap(self, a, depth):
         tree_size = 2 * 2**depth - 1
         self.HeapArray = [None] * tree_size
+        if not a:
+            return []
         for i in a:
             self.Add(i)
         return self.HeapArray
 
     def GetMax(self):
-        # вернуть значение корня и перестроить кучу
-        return -1  # если куча пуста
+        if len(self.HeapArray) == 0:
+            return -1
+
+        max = self.HeapArray[0]
+        self.HeapArray[0] = self.HeapArray[-1]
+        self.HeapArray[-1] = None
+
+        self.Restruct(0)
+
+        return max
+
+    def Restruct(self, index):
+        left_child_index = self.GetLeftChildIndex(index)
+        right_child_index = self.GetRightChildIndex(index)
+        if left_child_index >= len(self.HeapArray) and right_child_index >= len(self.HeapArray):
+            return
+        current_element = self.HeapArray[index]
+        left_child = self.HeapArray[left_child_index]
+        right_child = self.HeapArray[right_child_index]
+        if left_child < current_element and right_child < current_element:
+            return
+
+        if left_child > current_element and right_child > current_element:
+            next_index = left_child_index if left_child > right_child else right_child_index
+        if left_child > current_element > right_child:
+            next_index = left_child_index
+        if left_child < current_element < right_child:
+            next_index = right_child_index
+
+        self.HeapArray[index], self.HeapArray[next_index] = self.HeapArray[next_index], self.HeapArray[index]
+
+        self.Restruct(next_index)
+
+    def GetLeftChildIndex(self, index):
+        return 2 * index + 1
+
+    def GetRightChildIndex(self, index):
+        return 2 * index + 2
 
     def Add(self, key):
         # добавляем новый элемент key в кучу и перестраиваем её
         if None not in self.HeapArray:
             return False
         index = self.HeapArray.index(None)
-        parent_index = (index-1)//2
-        left_child_index = 2 * index + 1
-        right_child_index = 2 * index + 2
         self.HeapArray[index] = key
         if index == 0:
             return True
-
-        while self.HeapArray[index-1] is not None and key > self.HeapArray[parent_index]:
+        parent_index = (index-1)//2
+        while self.HeapArray[parent_index] < key:
             self.HeapArray[index] = self.HeapArray[parent_index]
             self.HeapArray[parent_index] = key
-            index = index-1
-
+            index = parent_index
+            parent_index = (parent_index-1)//2
+            if parent_index < 0:
+                parent_index = 0
         
+        current_index = self.HeapArray.index(key)
+        left_child_index = 2 * current_index + 1
+        right_child_index = 2 * current_index + 2
 
+        try:
+            if self.HeapArray[left_child_index] is not None and self.HeapArray[left_child_index] > key:
+                self.HeapArray[current_index] = self.HeapArray[left_child_index]
+                self.HeapArray[left_child_index] = key
+        except IndexError:
+            pass
 
-"""
-Аналогично выполняется и процесс вставки. 
-Новый элемент помещаем в самый низ массива, в первый свободный слот, и затем поднимаем его вверх по дереву, 
-останавливаясь в позиции, когда выше у родителя будет больший ключ, а ниже у обоих наследников -- меньшие.
-"""
-# while index < len(self.Tree):
-#     node = self.Tree[index]
-#     if node is None:
-#         self.Tree[index] = key
-#         return index
-#     if key == node:
-#         return index
-#     if key < node:
-#         index = 2 * index + 1
-#     if key > node:
-#         index = 2 * index + 2
-# return -1
+        try:
+            if self.HeapArray[right_child_index] is not None and self.HeapArray[right_child_index] > key:
+                self.HeapArray[current_index] = self.HeapArray[right_child_index]
+                self.HeapArray[right_child_index] = key
+        except IndexError:
+            pass
--- als2/tests/test_heap.py
+++ als2/tests/test_heap.py
@@ -1,8 +1,56 @@
+import unittest
+
 from heap import Heap
 
-def test_create_heap_with_7_nodes():
-    test_arr = [1, 2, 6, 4, 8, 5, 3]
 
-    heap = Heap().MakeHeap(test_arr, 2)
+class TestHeap(unittest.TestCase):
 
-    assert [8, 6, 5, 1, 4, 2, 3] == heap
+    def test_create_heap_empty_arr(self):
+        test_arr = []
+
+        heap = Heap().MakeHeap(test_arr, 0)
+
+        self.assertEqual([], heap)
+
+    def test_create_heap_with_7_nodes(self):
+        test_arr = [1, 2, 6, 4, 8, 5, 3]
+
+        heap = Heap().MakeHeap(test_arr, 2)
+
+        self.assertEqual([8, 6, 5, 1, 4, 2, 3], heap)
+
+    def test_create_heap_with_one_node(self):
+        test_arr = [5]
+
+        heap = Heap().MakeHeap(test_arr, 0)
+
+        self.assertEqual([5], heap)
+
+    def test_get_max_value_from_2_level_heap(self):
+        heap = Heap()
+        heap.MakeHeap([3, 6, 1, 8, 5, 2, 4], 2)
+
+        self.assertEqual([8, 6, 4, 3, 5, 1, 2], heap.HeapArray)
+        max = heap.GetMax()
+
+        self.assertEqual(8, max)
+        self.assertEqual([6, 5, 4, 3, 2, 1, None], heap.HeapArray)
+
+    def test_find_max_in_empty_heap(self):
+        heap = Heap()
+
+        self.assertEqual(-1, heap.GetMax())
+
+    def test_add_node_to_heap(self):
+        heap = Heap()
+        heap.HeapArray = [6, 5, 4, 3, 2, 1, None]
+
+        heap.Add(8)
+
+        self.assertEqual([8, 5, 6, 3, 2, 1, 4], heap.HeapArray)
+
+    def test_add_node_to_full_heap(self):
+        heap = Heap()
+        heap.HeapArray = [6, 5, 4, 3, 2, 1]
+
+        self.assertFalse(heap.Add(12))
