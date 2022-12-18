350b45d2ecccd1b3997ed9717c5f9d9f2cc59cbb
Lint Action <lint-action@samuelmeuli.com>
2022-12-18 08:09:13 +0000

    Fix code style issues with Black

--- als2/graph.py
+++ als2/graph.py
@@ -1,15 +1,14 @@
 class Vertex:
-
     def __init__(self, val):
         self.Value = val
-  
+
+
 class SimpleGraph:
-	
     def __init__(self, size):
         self.max_vertex = size
         self.m_adjacency = [[0] * size for _ in range(size)]
         self.vertex = [None] * size
-        
+
     def AddVertex(self, v):
         if None not in self.vertex:
             return False
@@ -21,10 +20,10 @@ class SimpleGraph:
         for i in self.m_adjacency:
             i[v] = 0
         self.m_adjacency[v] = [0] * self.max_vertex
-	
+
     def IsEdge(self, v1, v2):
         return True if self.m_adjacency[v1][v2] else False
-	
+
     def AddEdge(self, v1, v2):
         self.m_adjacency[v1][v2] = 1
         self.m_adjacency[v2][v1] = 1
