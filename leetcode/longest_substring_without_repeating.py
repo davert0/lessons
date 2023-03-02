c6add1ba1f44b024c95061924eb007d063bb06ca
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2023-03-02 10:01:22 +0300

    add longest substing

---
+++ leetcode/longest_substring_without_repeating.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = []
+        indexes = {}
+        longest = 0
+        i = 0
+
+        while i < len(s):
+            if s[i] not in chars:
+                chars.append(s[i])
+                indexes[s[i]] = i
+                i += 1
+            else:
+                if len(chars) > longest:
+                    longest = len(chars)
+                chars = []
+                i = indexes[s[i]] + 1
+
+        return longest
+
+sol = Solution()
+sol.lengthOfLongestSubstring("abcabcbb")
